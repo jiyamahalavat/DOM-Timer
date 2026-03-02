d94add219dbac595f9be5b8ac1a95318d505fd6c
jiyamahalavat <jiyamahalavat@gmail.com>
2026-03-02 12:43:30 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
-let timer = 0;
+et timer = 0;
+let intervalId = null;
 
-document.getElementById("startTimer").onclick
+// Start Timer
+document.getElementById("startTimer").onclick = function() {
+
+    // Prevent multiple intervals from running
+    if (intervalId === null) {
+        intervalId = setInterval(function() {
+            timer++;
+            document.getElementById("timerDisplay").innerHTML = timer;
+        }, 1000);
+    }
+};
+
+// Stop Timer
+document.getElementById("stopTimer").onclick = function() {
+    clearInterval(intervalId);
+    intervalId = null;
+};
+
+// Reset Timer
+document.getElementById("resetTimer").onclick = function() {
+    clearInterval(intervalId);
+    intervalId = null;
+    timer = 0;
+    document.getElementById("timerDisplay").innerHTML = timer;
+};
+
+
